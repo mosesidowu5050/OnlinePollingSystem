0ea36898a27c313842b6a8a0414ce3daf4064536
idowu moses <dynamopraize5050@gmail.com>
2025-07-02 05:12:14 +0100

    first commit

---
+++ src/main/java/org/mosesidowu/onlinepollingsystem/OnlinePollingSystemApplication.java
@@ -0,0 +1,13 @@
+package org.mosesidowu.onlinepollingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePollingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinePollingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mosesidowu/onlinepollingsystem/OnlinePollingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mosesidowu.onlinepollingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePollingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
