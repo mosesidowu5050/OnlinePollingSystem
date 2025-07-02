b19d7284dd90ad50eb2a80b573e46a37f1c1bbf8
idowu moses <dynamopraize5050@gmail.com>
2025-07-02 19:40:36 +0100

    update project

---
+++ src/main/java/org/mosesidowu/onlinepollingsystem/security/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package org.mosesidowu.onlinepollingsystem.security;
+
+
+import jdk.jshell.spi.ExecutionControl;
+import org.mosesidowu.onlinepollingsystem.data.models.User;
+import org.mosesidowu.onlinepollingsystem.data.repository.UserRepository;
+import org.mosesidowu.onlinepollingsystem.exception.UserException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findUsersByEmail(email)
+                .orElseThrow(() -> new UserException("User not found"));
+
+        List<GrantedAuthority> authorities = List.of(
+                new SimpleGrantedAuthority(user.getRole().name())
+        );
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getEmail(),
+                authorities
+        );
+    }
+}
