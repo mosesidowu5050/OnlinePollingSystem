5ee050e0ec0396fbb674393fc91e1f2c6f777dce
idowu moses <dynamopraize5050@gmail.com>
2025-07-04 11:32:50 +0100

    upated project

--- src/main/java/org/mosesidowu/onlinepollingsystem/controller/AuthController.java
+++ src/main/java/org/mosesidowu/onlinepollingsystem/controller/AuthController.java
@@ -1,14 +1,9 @@
 package org.mosesidowu.onlinepollingsystem.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.mosesidowu.onlinepollingsystem.services.IUserService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -16,23 +11,6 @@ import java.util.Map;
 public class AuthController {
 
 
-    @GetMapping("/status")
-    public ResponseEntity<Map<String, String>> getAuthStatus() {
-        Map<String, String> response = new HashMap<>();
-        response.put("status", "Authentication service is operational.");
-        response.put("googleLoginUrl", "/oauth2/authorization/google");
-        response.put("logoutUrl", "/api/auth/logout");
-
-        return ResponseEntity.ok(response);
-    }
 
 
-    @GetMapping("/google-login-url")
-    public ResponseEntity<Map<String, String>> getGoogleLoginUrl() {
-        Map<String, String> response = new HashMap<>();
-        response.put("url", "/oauth2/authorization/google");
-        response.put("description", "Login through your Google email.");
-
-        return ResponseEntity.ok(response);
-    }
 }
